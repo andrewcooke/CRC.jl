85e63caf130e9ee58a33583ad52199efd2d28d63
andrew cooke <andrew@acooke.org>
2014-03-29 11:40:01 -0300

    untested slow table

--- src/CRC.jl
+++ src/CRC.jl
@@ -14,20 +14,25 @@ export rem_no_table
 # http://en.wikipedia.org/wiki/Computation_of_cyclic_redundancy_checks
 # - here we implement the "byte at a time" approach, without tables.
 
+function check_generator{G<:Unsigned}(degree::Int, generator::G, word_size::Int)
+    @assert degree <= 8 * sizeof(G) "generator too small for degree"
+    shift = degree - word_size
+    @assert shift >= 0 "polynomial smaller than data chunk"
+    # this is carry before shift, so is (implicit) msb / 2
+    carry = one(G) << (degree - 1)
+    mask = convert(G, (1 << degree) - 1)
+    (generator & mask, shift, carry, mask)
+end
+
 # we are careful to allow a missing msb in the generator, since that allows
 # 8th degree polynomials to be specified in 8 bits, etc.
 function rem_no_table{G<:Unsigned,D<:Unsigned}(degree::Int, generator::G, data::Vector{D})
-    mask = convert(G, (1 << degree) - 1)
-    # this is carry before shift, so is (implicit) msb / 2
-    carry = one(G) << (degree - 1)
     word_size = 8 * sizeof(D)
-    shift = degree - word_size
-    @assert degree <= 8 * sizeof(G) "generator too small for degree"
-    @assert shift >= 0 "polynomial smaller than data chunk"
+    generator, shift, carry, mask = check_generator(degree, generator, word_size)
     remainder::G = zero(G)
     for word in data
         remainder = remainder $ (convert(G, word) << shift)
-        for i in 1:word_size
+        for _ in 1:word_size
             if remainder & carry == carry
                 remainder = (remainder << 1) $ generator
             else
@@ -36,9 +41,42 @@ function rem_no_table{G<:Unsigned,D<:Unsigned}(degree::Int, generator::G, data::
         end
     end
     # when the generator is smaller than the data type we don't lose
-    # bits by overflow, so trim before returning (no need to discard
-    # optional explicit msb before loop because this does the same)
+    # bits by overflow, so trim before returning
     remainder & mask
+end
+
+
+# generate a word-sized lookup table to optimize the word_size loop in
+# the routine above.
+
+function single_table{G<:Unsigned}(degree::Int, generator::G, word_size::Int)
+    @assert word_size < 33 "table too large"  # even this is huge
+    generator, shift, carry, mask = check_generator(degree, generator, word_size)
+    size = 2 ^ word_size
+    table = Array(G, size)
+    for word in 0:(size-1)
+        remainder::G = convert(G, word << shift)
+        for _ in 1:word_size
+            if remainder & carry == carry
+                remainder = (remainder << 1) $ generator
+            else
+                remainder = remainder << 1
+            end
+        end
+        table[word+1] = remainder
+    end
+    table
+end
+
+function rem_single_table{G<:Unsigned,D<:Unsigned}(degree::Int, generator::G, data::Vector{D}, table::Vector{G})
+    word_size = 8 * sizeof(D)
+    generator, shift, mask, carry = check_generator(degree, generator, word_size)
+    remainder::G = zero(G)
+    for word in data
+        remainder = remainder $ (convert(G, word) << shift)
+        remainder = table[(remainder >> shift) + 1]
+    end
+    remainder
 end
 
 
