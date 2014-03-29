bf0f878f16f753b5dcb51fbc4d0375a7cd11e02d
andrew cooke <andrew@acooke.org>
2014-03-28 22:24:00 -0300

    initial code

--- src/CRC.jl
+++ src/CRC.jl
@@ -1,5 +1,45 @@
+
+# calculate crc checksums for data.  this process is closely related
+# to the routines in IntModN (particularly GF2Poly), but the
+# traditional implementations are optimized to the point where
+# implementing them from "first principles" makes little sense
+# (although we can use IntModN to check the results here).
+
 module CRC
 
-# package code goes here
+export rem_no_table
 
-end # module
+# the basic idea is to do a polynomial division along the byte stream.
+# it's all explained very nicely at
+# http://en.wikipedia.org/wiki/Computation_of_cyclic_redundancy_checks
+# - here we implement the "byte at a time" approach, without tables.
+
+# we are careful to allow a missing msb in the generator, since that allows
+# 8th degree polynomials to be specified in 8 bits, etc.
+function rem_no_table{G<:Unsigned,D<:Unsigned}(degree::Int, generator::G, data::Vector{D})
+    mask = convert(G, (1 << degree) - 1)
+    carry = one(G) << (degree - 1)
+    println("mask $mask carry $carry")
+    generator &= mask
+    word_size = 8 * sizeof(D)
+    shift = degree - word_size
+    @assert degree <= 8 * sizeof(G) "generator too small for degree"
+    @assert shift >= 0 "polynomial smaller than data chunk"
+    remainder::G = zero(G)
+    for word in data
+        remainder = remainder $ (convert(G, word) << shift)
+        println("remainder $remainder")
+        for i in 1:word_size
+            if remainder & carry == carry
+                remainder = (remainder << 1) $ generator
+            else
+                remainder <<= 1
+            end
+        end
+    end
+    println("remainder $remainder")
+    remainder
+end
+
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,26 @@
 using CRC
-using Base.Test  
+using Base.Test
+using IntModN
 
-# write your own tests here
-@test 1 == 1
+function test_rem_no_table()
+    for _ in 1:100
+        a1 = rand(Uint8, 2)
+        b1 = rand(Uint8)
+        println("a1 $(a1[1]) $(a1[2]) b1 $b1")
+        a2 = convert(Uint64, convert(Uint64, a1[1]) << 16 + convert(Uint64, a1[2]) << 8)
+        b2 = convert(Uint64, (1 << 8) | b1)
+        println("a2 $a2 b2 $b2")
+        c1 = rem_no_table(8, b1, a1)
+        c2 = GF2Poly(a2) % GF2Poly(b2)
+        println("c1 $c1 c2 $c2 ($(c2.i))")
+        @test c2.i == c1
+        @test c1 < b2
+        @test c2.i < b2
+    end
+    println("rem_no_table ok")
+end
+
+
+srand(0)
+test_rem_no_table()
+
